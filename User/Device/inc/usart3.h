d83d17f5c75b7079841b5810416cbe0b9a2450cc
Bashpow <B31296637828@hotmail.com>
2021-09-26 22:26:42 +0800

    加入printf的支持

--- User/Device/inc/usart3.h
+++ User/Device/inc/usart3.h
@@ -1,10 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-26 14:07:44
+ * @LastEditTime: 2021-09-26 22:22:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \MDK-ARMc:\Document\Workspace\Hero2022\User\Device\inc\usart3.h
+ */
 #ifndef __USART3_TASK_H_
 #define __USART3_TASK_H_
 
+#include <stdarg.h>
+#include "stdio.h"
 #include "usart.h"
 
 void Usart3_Tx_Init(void);
 void Usart3_Transmit_Dma(uint32_t data_address, uint32_t len);
 void Usart3_It_Tc_Callback(void);
+int __printf(const char *format, ...);
 
 #endif //  __USART3_TASK_H_
--- User/Device/src/usart3.c
+++ User/Device/src/usart3.c
@@ -1,5 +1,8 @@
 #include "usart3.h"
 
+static uint8_t usart3_tx_lock = 0;
+static char usart3_dma_transmit_buf[256];
+
 /**
  * @description: 串口3的额外初始化
  * @param {*}
@@ -19,12 +22,16 @@ void Usart3_Tx_Init(void)
  */
 void Usart3_Transmit_Dma(uint32_t data_address, uint32_t len)
 {
-	LL_DMA_SetMemoryAddress(DMA1, LL_DMA_STREAM_3, data_address);
-	LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_3, len);
-	LL_USART_ClearFlag_TC(USART3);
-	LL_DMA_ClearFlag_TC3(DMA1);
-	LL_USART_EnableIT_TC(USART3);
-	LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_3);
+	if(usart3_tx_lock == 0)
+	{
+		usart3_tx_lock = 1;
+		LL_DMA_SetMemoryAddress(DMA1, LL_DMA_STREAM_3, data_address);
+		LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_3, len);
+		LL_USART_ClearFlag_TC(USART3);
+		LL_DMA_ClearFlag_TC3(DMA1);
+		LL_USART_EnableIT_TC(USART3);
+		LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_3);
+	}
 }
 
 /**
@@ -34,7 +41,18 @@ void Usart3_Transmit_Dma(uint32_t data_address, uint32_t len)
  */
 void Usart3_It_Tc_Callback(void)
 {
-	//TODO:解锁print互斥
+	usart3_tx_lock = 0;
 	LL_USART_DisableIT_TC(USART3);
 	LL_DMA_DisableStream(DMA1, LL_DMA_STREAM_3);
+}
+
+int __printf(const char *format, ...)
+{
+	uint32_t len;
+	va_list args;
+	va_start(args, format);
+	len = vsnprintf((char*)usart3_dma_transmit_buf,sizeof(usart3_dma_transmit_buf),(char*)format,args);
+	va_end(args);
+	Usart3_Transmit_Dma((uint32_t)usart3_dma_transmit_buf, len);
+	return 1;
 }
