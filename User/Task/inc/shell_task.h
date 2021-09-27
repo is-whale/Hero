3e571b33a9de0de3725afbd71e4267c562394d3b
Bashpow <B31296637828@hotmail.com>
2021-09-27 16:56:24 +0800

    删除多余代码，增加初始化函数

--- User/Task/inc/shell_task.h
+++ User/Task/inc/shell_task.h
@@ -9,12 +9,8 @@
 #include "stm32f4xx_ll_usart.h."
 #include "usart3.h"
 
-#define PRINT_USART_INIT() Usart3_Rx_Init()
+#define PRINT_USART_INIT() {Usart3_Tx_Init();Usart3_Rx_Init();}
 
 void Usart3_Idle_ITCallback(void);
-void Usart_Send_Byte(USART_TypeDef *pUsart, uint8_t ch);
-void Usart_Send_String(USART_TypeDef *pUsart,char *str);
-void Usart_Send_HalfWord(USART_TypeDef *pUsart, uint16_t ch);
-void Usart_Send_ArrayBuffer(USART_TypeDef *pUsart, uint8_t *array, uint16_t length);
 
 #endif // __SHELL_TASK_
