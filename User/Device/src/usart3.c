70f84fbbcb073a960745a3dc198a2190fe620369
Bashpow <B31296637828@hotmail.com>
2021-09-27 18:13:57 +0800

    修复printf的问题

--- User/Device/src/usart3.c
+++ User/Device/src/usart3.c
@@ -1,7 +1,7 @@
 #include "usart3.h"
 
 static volatile uint8_t usart3_tx_lock = 0;
-static char usart3_dma_transmit_buf[256];
+static volatile char usart3_dma_transmit_buf[256];
 static const uint16_t usart3_dma_rx_max_len = 50;
 volatile uint8_t usart3_dma_rx_buffer[usart3_dma_rx_max_len];
 
@@ -18,28 +18,18 @@ void Usart3_Tx_Init(void)
 
 /**
  * @brief 串口3使用DMA方式发送
- * @note 会占用红灯，当等待发送时红灯亮
  * @param {uint32_t} data_address 发送的数据地址
  * @param {uint32_t} len 发送的数据长度
  * @return {*}
  */
 void Usart3_Transmit_Dma(uint32_t data_address, uint32_t len)
 {
-	while(usart3_tx_lock)
-	{
-		LED_RED_ON();
-	}
-	if(usart3_tx_lock == 0)
-	{
-		usart3_tx_lock = 1;
-		LL_DMA_SetMemoryAddress(DMA1, LL_DMA_STREAM_3, data_address);
-		LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_3, len);
-		LL_USART_ClearFlag_TC(USART3);
-		LL_DMA_ClearFlag_TC3(DMA1);
-		LL_USART_EnableIT_TC(USART3);
-		LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_3);
-		LED_RED_OFF();
-	}
+	LL_DMA_SetMemoryAddress(DMA1, LL_DMA_STREAM_3, data_address);
+	LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_3, len);
+	LL_USART_ClearFlag_TC(USART3);
+	LL_DMA_ClearFlag_TC3(DMA1);
+	LL_USART_EnableIT_TC(USART3);
+	LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_3);
 }
 
 /**
@@ -81,16 +71,27 @@ const uint16_t *Get_Usart3_DMA_Rx_MaxLen(void)
 
 /**
  * @brief printf函数（不建议直接调用）
+ * @note 会占用红灯，当等待发送时红灯亮
  * @param {char} *format 同printf
  * @return {int} 打印的字符长度
  */
 int __printf(const char *format, ...)
 {
-	uint32_t len;
-	va_list args;
-	va_start(args, format);
-	len = vsnprintf((char*)usart3_dma_transmit_buf,sizeof(usart3_dma_transmit_buf),(char*)format,args);
-	va_end(args);
-	Usart3_Transmit_Dma((uint32_t)usart3_dma_transmit_buf, len);
-	return len;
+	while (usart3_tx_lock)
+	{
+		LED_RED_ON();
+	}
+	if (usart3_tx_lock == 0)
+	{
+		usart3_tx_lock = 1;
+		uint32_t len;
+		va_list args;
+		va_start(args, format);
+		len = vsnprintf((char *)usart3_dma_transmit_buf, sizeof(usart3_dma_transmit_buf), (char *)format, args);
+		va_end(args);
+		Usart3_Transmit_Dma((uint32_t)usart3_dma_transmit_buf, len);
+		LED_RED_OFF();
+		return len;
+	}
+	return -1;
 }
