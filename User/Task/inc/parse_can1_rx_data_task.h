cdfe4d8c8bb20cf2ed76f126ddb11e4eab81c56a
weihao <weihao_ysgs@163.com>
2021-09-28 21:58:04 +0800

    初步完成 can1 数据解析任务，待测试

--- User/Task/inc/parse_can1_rx_data_task.h
+++ User/Task/inc/parse_can1_rx_data_task.h
@@ -4,5 +4,11 @@
 #include "cmsis_os.h"
 #include "stm32f4xx.h"
 #include "usart3.h"
+#include "motor.h"
+#include "can1.h"
+#include "can1_device.h"
+
+void Info_Can1_ParseData_Task(void);
+void Parse_Can1_Rxd_Data(void);
 
 #endif //__PARSE_CAN1_RX_DATA_TASK_H_
--- User/Task/src/parse_can1_rx_data_task.c
+++ User/Task/src/parse_can1_rx_data_task.c
@@ -2,21 +2,24 @@
 
 extern osThreadId parseCan1RxDataHandle;
 static const uint16_t can1_get_data_signal = 0x0001;
+Motor_Measure_t m3508_feddback_data[4];
 
 void StartParseCan1RxDataTask(void const *argument)
 {
     osEvent can1_get_data_event;
 
+    Can1_Filter_Init();
+
     osDelay(100);
 
     for (;;)
     {
-        can1_get_data_event = osSignalWait(can1_get_data_signal,can1_get_data_signal);
+        can1_get_data_event = osSignalWait(can1_get_data_signal, can1_get_data_signal);
         if (can1_get_data_event.status == osEventSignal)
         {
             if (can1_get_data_event.value.signals == can1_get_data_signal)
             {
-                // TODO 解析 can1 接收的数据
+                Parse_Can1_Rxd_Data();
             }
         }
         else if (can1_get_data_event.status == osEventTimeout)
@@ -26,7 +29,17 @@ void StartParseCan1RxDataTask(void const *argument)
     }
 }
 
-void Info_Can1_ParseData_Task()
+void Parse_Can1_Rxd_Data(void)
 {
-    osSignalSet(parseCan1RxDataHandle,can1_get_data_signal);
+    CAN_RxHeaderTypeDef can1_rx_header;
+    static uint8_t i = 0;
+    static uint8_t can1_rx_data[8];
+    HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &can1_rx_header, can1_rx_data);
+    i = can1_rx_header.StdId - CAN_3508_M1_ID;
+    GET_MOTOR_MEASURE_DATA(&m3508_feddback_data[i], can1_rx_data);
+}
+
+void Info_Can1_ParseData_Task(void)
+{
+    osSignalSet(parseCan1RxDataHandle, can1_get_data_signal);
 }
