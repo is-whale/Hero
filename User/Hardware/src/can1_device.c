bd76dc70b0132ac7728cc9e6c71b4b3880a82503
weihao <weihao_ysgs@163.com>
2021-09-28 21:40:11 +0800

    更新can1_device.h.c 的相关定义

--- User/Hardware/src/can1_device.c
+++ User/Hardware/src/can1_device.c
@@ -1,18 +1,1 @@
-#ifndef __CAN1_DEVICE_H_
-#define __CAN1_DEVICE_H_
-
-#include "stm32f4xx.h"
-
-typedef enum
-{
-    CAN_CHASSIS_ALL_ID = 0x200,
-    CAN_3508_M1_ID = 0x201,
-    CAN_3508_M2_ID = 0x202,
-    CAN_3508_M3_ID = 0x203,
-    CAN_3508_M4_ID = 0x204,
-
-    SUPER_CAPACITOR_ID = 0x211,
-    SUPER_CAPACITOR_SEND_ID = 0x210,
-} Can1_RxMsg_ID;
-
-#endif // __CAN1_DEVICE_H_
+#include "can1_device.h"
