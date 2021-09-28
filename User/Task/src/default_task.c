4dd6578e9a071b60e899e750d163945cf37c9290
Bashpow <B31296637828@hotmail.com>
2021-09-28 13:52:08 +0800

    更改灯光设置

--- User/Task/src/default_task.c
+++ User/Task/src/default_task.c
@@ -2,32 +2,9 @@
 
 void StartDefaultTask(void const *argument)
 {
-	Led_Flow_Flash(); //暂时放这
-
-	/* 关闭所有LED */
+	/* 关闭红绿LED */
 	LED_GREEN_OFF();
 	LED_RED_OFF();
-
-	/* 炫酷的闪烁后关闭 */
-	LED_GREEN_ON();
-	osDelay(5);
-	LED_GREEN_OFF();
-	LED_RED_ON();
-	osDelay(5);
-	LED_RED_OFF();
-	osDelay(5);
-	LED_GREEN_ON();
-	LED_RED_ON();
-	osDelay(5);
-	LED_GREEN_OFF();
-	LED_RED_OFF();
-	osDelay(5);
-	LED_GREEN_ON();
-	LED_RED_ON();
-	osDelay(5);
-	LED_GREEN_OFF();
-	LED_RED_OFF();
-	osDelay(5);
 
 	for (;;)
 	{
