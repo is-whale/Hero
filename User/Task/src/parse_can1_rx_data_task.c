8312fe5541a55f5ff4ebccab3588357b98280230
weihao <weihao_ysgs@163.com>
2021-09-29 13:15:48 +0800

    完成 CAN1 的数据解析任务，测试通过

--- User/Task/src/parse_can1_rx_data_task.c
+++ User/Task/src/parse_can1_rx_data_task.c
@@ -5,36 +5,31 @@ static const uint16_t can1_get_data_signal = 0x0001;
 static const uint8_t can1_motor_device_number = 4;
 static const uint8_t can1_rx_data_overtime = 100;
 static Motor_Measure_t m3508_feddback_data[can1_motor_device_number];
+static uint8_t *can1_rxd_data;
+static CAN_RxHeaderTypeDef *can1_rx_header;
 
-void StartParseCan1RxDataTask(void const * argument)
+void StartParseCan1RxDataTask(void const *argument)
 {
-    // debug_print("cansig\r\n");
     osEvent can1_get_data_event;
 
     Can1_Filter_Init();
+    can1_rxd_data = Get_CAN1_Rxd_Data();
+    can1_rx_header = Get_CAN1_Rx_Header();
 
-    osDelay(100);
+    osDelay(1000);
+
     HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
-    // debug_print("cansig\r\n");
+
     for (;;)
     {
         osDelay(10);
-        debug_print("cansig\r\n");
         can1_get_data_event = osSignalWait(can1_get_data_signal, can1_rx_data_overtime);
-        // debug_print("cansig\r\n");
         if (can1_get_data_event.status == osEventSignal)
         {
-            debug_print("cansignal\r\n");
             if (can1_get_data_event.value.signals == can1_get_data_signal)
             {
-                debug_print("cansssss   ig\r\n");
                 Parse_Can1_Rxd_Data();
-                debug_showdata1("mechine angle", m3508_feddback_data[0].mechanical_angle);
-                debug_showdata1("speed", m3508_feddback_data[0].speed_rpm);
-                debug_showdata1("temperate", m3508_feddback_data[0].temperate);
-                osDelay(10);
             }
-        
         }
     }
 }
@@ -46,9 +41,7 @@ void StartParseCan1RxDataTask(void const * argument)
  */
 void Info_Can1_ParseData_Task(void)
 {
-    // debug_print("can1\r\n");
     osSignalSet(parseCan1RxDataHandle, can1_get_data_signal);
-    Parse_Can1_Rxd_Data();
 }
 
 /**
@@ -58,25 +51,15 @@ void Info_Can1_ParseData_Task(void)
  */
 void Parse_Can1_Rxd_Data(void)
 {
-    CAN_RxHeaderTypeDef can1_rx_header;
     static uint8_t i = 0;
-    static uint8_t can1_rx_data[8];
-    // debug_print("can2\r\n");
-    HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &can1_rx_header, can1_rx_data);
-    // debug_print("can3\r\n");
-    i = can1_rx_header.StdId - CAN_3508_M1_ID;
-    // debug_showdata1("id", i);
-    GET_MOTOR_MEASURE_DATA(&(m3508_feddback_data[i]), can1_rx_data);
-    // debug_showdata1("id", i+1);
-    // debug_showdata1("mechine angle", m3508_feddback_data[0].mechanical_angle);
-    // debug_showdata1("speed", m3508_feddback_data[0].speed_rpm);
-    // debug_showdata1("temperate", m3508_feddback_data[0].temperate);
+    i = can1_rx_header->StdId - CAN_3508_M1_ID;
+    Calculate_Motor_Data(&m3508_feddback_data[i], can1_rxd_data);
 }
 
 /**
- * @brief           返回 CAN1 总线上电机的反馈数据
- * @param[in]       void
- * @return          Motor_Measure_t* 反馈电机数据结构体指针
+ * @brief                       返回 CAN1 总线上电机的反馈数据
+ * @param[in]                   void
+ * @return {Motor_Measure_t*}   反馈电机数据结构体指针
  */
 Motor_Measure_t *Get_Can1_Feedback_Data(void)
 {
@@ -84,9 +67,9 @@ Motor_Measure_t *Get_Can1_Feedback_Data(void)
 }
 
 /**
- * @brief           返回 CAN1 总线上电机的数量
- * @param[in]       void
- * @return          const uint8_t* CAN1 总线上电机数量
+ * @brief                     返回 CAN1 总线上电机的数量
+ * @param[in]                 void
+ * @return {const uint8_t*}   CAN1 总线上电机数量
  */
 const uint8_t *Get_Can1_Motor_DeviceNumber(void)
 {
