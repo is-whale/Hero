dc3536369a0a2deb3e9477ba678309726f4ce7a4
weihao <weihao_ysgs@163.com>
2021-10-01 19:36:39 +0800

    完成云台基本框架，完成TODO

--- User/Task/src/gimbal_task.c
+++ User/Task/src/gimbal_task.c
@@ -1,21 +1,22 @@
 #include "gimbal_task.h"
 
-static Motor_Measure_t *pitch_motor_feedback_parsed_data;
-static Motor_Measure_t *yaw_motor_feedback_parsed_data;
+static const uint8_t gimbal_motor_num = 2;
+static const uint8_t yaw_motor_index = 0;
+static const uint8_t pitch_motor_index = 1;
+
 static CAN_RxHeaderTypeDef *can2_rx_header_pt;
 static uint8_t *can2_rxd_data_buffer;
 
 static Rc_Ctrl_t *rc_data_pt;
 static Robot_control_data_t *robot_mode_data_pt;
 
-static Motor_Measure_t pitch_motor_parsed_feedback_data;
+static Motor_Measure_t gimbal_motor_parsed_feedback_data[gimbal_motor_num];
+
 
 void StartGimbalTask(void const *argument)
 {
     float yaw_speed, pitch_speed;
     Can2_Filter_Init();
-    pitch_motor_feedback_parsed_data = Get_Pitch_Motor_Feedback_Data();
-    yaw_motor_feedback_parsed_data = Get_Yaw_Motor_Feedback_Data();
     can2_rx_header_pt = Get_CAN2_Rx_Header();
     can2_rxd_data_buffer = Get_CAN2_Rxd_Buffer();
     rc_data_pt = Get_Rc_Parsed_RemoteData_Pointer();
@@ -25,16 +26,12 @@ void StartGimbalTask(void const *argument)
     robot_mode_data_pt->mode.control_device = 0;
     robot_mode_data_pt->mode.rc_motion_mode = 1;
 
-    (void)yaw_motor_feedback_parsed_data;   ///< 暂时为了避免警告 unused warning
-    (void)pitch_motor_feedback_parsed_data; ///< 暂时为了避免警告 unused warning
-
     osDelay(1000);
     __OPEN_CAN2_RX_FIFO0_IT__;
 
     for (;;)
     {
-        // TODO 随后进行更改，改函数应解析所有的 CAN2 上云台电机的数据，此时最后一个参数这里只是为了测试
-        Parse_Can2_Gimbal_Rxd_Data(can2_rx_header_pt, can2_rxd_data_buffer, &pitch_motor_parsed_feedback_data);
+        Parse_Can2_Gimbal_Rxd_Data(can2_rx_header_pt, can2_rxd_data_buffer, gimbal_motor_parsed_feedback_data);
         if (robot_mode_data_pt->mode.control_device == 0)
         {
             switch (1)
@@ -54,7 +51,7 @@ void StartGimbalTask(void const *argument)
             }
         }
 
-        Set_Gimbal_Motors_Speed(yaw_speed, pitch_speed, &pitch_motor_parsed_feedback_data, &pitch_motor_parsed_feedback_data);
+        Set_Gimbal_Motors_Speed(yaw_speed, pitch_speed, &gimbal_motor_parsed_feedback_data[yaw_motor_index], &gimbal_motor_parsed_feedback_data[pitch_motor_index]);
         osDelay(10);
     }
 }
@@ -66,13 +63,13 @@ void Parse_Can2_Gimbal_Rxd_Data(CAN_RxHeaderTypeDef *p_can_rx_header, uint8_t *d
     {
     case CAN_YAW_MOTOR_ID:
     {
-        Calculate_Motor_Data(motor, data);
+        Calculate_Motor_Data(&gimbal_motor_parsed_feedback_data[yaw_motor_index], data);
         break;
     }
 
     case CAN_PITCH_MOTOR_ID:
     {
-        Calculate_Motor_Data(motor, data);
+        Calculate_Motor_Data(&gimbal_motor_parsed_feedback_data[pitch_motor_index], data);
         break;
     }
 
