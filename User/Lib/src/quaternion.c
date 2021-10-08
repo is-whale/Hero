cc41aef12a82c91aea60335be68cd894f0310abc
weihao <weihao_ysgs@163.com>
2021-10-08 14:00:07 +0800

    更新注释

--- User/Lib/src/quaternion.c
+++ User/Lib/src/quaternion.c
@@ -7,8 +7,8 @@ volatile float q1 = 0.0f;
 volatile float q2 = 0.0f;
 volatile float q3 = 0.0f;
 static volatile float gx, gy, gz, ax, ay, az, mx, my, mz;
-volatile uint32_t last_update, now_update; /* Sampling cycle count, ubit ms */
-volatile float exInt, eyInt, ezInt;        /* error integral */
+volatile uint32_t last_update, now_update; ///< Sampling cycle count, ubit ms
+volatile float exInt, eyInt, ezInt;        ///< error integral
 #define Kp 2.0f                            /*                                           \
                                             * proportional gain governs rate of         \
                                             * convergence to accelerometer/magnetometer \
@@ -17,13 +17,19 @@ volatile float exInt, eyInt, ezInt;        /* error integral */
                                             * integral gain governs rate of   \
                                             * convergence of gyroscope biases \
                                             */
+
+/**
+ * @brief               初始化四元数
+ * @param[in]           void
+ * @retval              void
+ */
 void Init_Quaternion(void)
 {
-    int16_t hx, hy; //hz;
+    int16_t hx, hy; ///< hz;
 
     hx = imu.mx;
     hy = imu.my;
-    //hz = imu.mz;
+    ///< hz = imu.mz;
 
 #ifdef BOARD_DOWN
     if (hx < 0 && hy < 0)
@@ -166,6 +172,11 @@ void Init_Quaternion(void)
 #endif
 }
 
+/**
+ * @brief           根据陀螺仪测的数据更新四元数
+ * @param[in]       void
+ * @retval          void
+ */
 void Imu_AHRS_Update(void)
 {
     float norm;
@@ -195,11 +206,11 @@ void Imu_AHRS_Update(void)
     my = imu.my;
     mz = imu.mz;
 
-    now_update = HAL_GetTick(); //ms
+    now_update = HAL_GetTick(); ///< ms
     halfT = ((float)(now_update - last_update) / 2000.0f);
     last_update = now_update;
 
-    /* Fast inverse square-root */
+    ///< Fast inverse square-root 
     norm = Inv_Sqrt(ax * ax + ay * ay + az * az);
     ax = ax * norm;
     ay = ay * norm;
@@ -215,14 +226,14 @@ void Imu_AHRS_Update(void)
     my = 0;
     mz = 0;
 #endif
-    /* compute reference direction of flux */
+    ///< compute reference direction of flux 
     hx = 2.0f * mx * (0.5f - q2q2 - q3q3) + 2.0f * my * (q1q2 - q0q3) + 2.0f * mz * (q1q3 + q0q2);
     hy = 2.0f * mx * (q1q2 + q0q3) + 2.0f * my * (0.5f - q1q1 - q3q3) + 2.0f * mz * (q2q3 - q0q1);
     hz = 2.0f * mx * (q1q3 - q0q2) + 2.0f * my * (q2q3 + q0q1) + 2.0f * mz * (0.5f - q1q1 - q2q2);
     bx = sqrt((hx * hx) + (hy * hy));
     bz = hz;
 
-    /* estimated direction of gravity and flux (v and w) */
+    ///< estimated direction of gravity and flux (v and w) 
     vx = 2.0f * (q1q3 - q0q2);
     vy = 2.0f * (q0q1 + q2q3);
     vz = q0q0 - q1q1 - q2q2 + q3q3;
@@ -238,7 +249,7 @@ void Imu_AHRS_Update(void)
     ey = (az * vx - ax * vz) + (mz * wx - mx * wz);
     ez = (ax * vy - ay * vx) + (mx * wy - my * wx);
 
-    /* PI */
+    ///< PI 
     if (ex != 0.0f && ey != 0.0f && ez != 0.0f)
     {
         exInt = exInt + ex * Ki * halfT;
@@ -255,7 +266,7 @@ void Imu_AHRS_Update(void)
     tempq2 = q2 + (q0 * gy - q1 * gz + q3 * gx) * halfT;
     tempq3 = q3 + (q0 * gz + q1 * gy - q2 * gx) * halfT;
 
-    /* normalise quaternion */
+    ///< normalise quaternion 
     norm = Inv_Sqrt(tempq0 * tempq0 + tempq1 * tempq1 + tempq2 * tempq2 + tempq3 * tempq3);
     q0 = tempq0 * norm;
     q1 = tempq1 * norm;
@@ -264,17 +275,17 @@ void Imu_AHRS_Update(void)
 }
 
 /**
-  * @brief  update imu attitude
-  * @param  
-  * @retval 
+  * @brief          update imu attitude
+  * @param          void
+  * @retval         void
   */
 void Imu_Attitude_Update(void)
 {
-    /* yaw    -pi----pi */
+    ///< yaw    -pi----pi 
     imu.yaw = -atan2(2 * q1 * q2 + 2 * q0 * q3, -2 * q2 * q2 - 2 * q3 * q3 + 1) * 57.3;
-    /* pitch  -pi/2----pi/2 */
+    ///< pitch  -pi/2----pi/2 
     imu.pit = -asin(-2 * q1 * q3 + 2 * q0 * q2) * 57.3;
-    /* roll   -pi----pi  */
+    ///< roll   -pi----pi  
     imu.rol = atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2 * q2 + 1) * 57.3;
     // debug_print("yaw %.2f, pitch %.2f, rol %.2f \r\n", imu.yaw, imu.pit, imu.rol);
 }
