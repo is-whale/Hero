d052cd1d6e00ac525d40eed158e6f83f85d5ea0d
weihao <weihao_ysgs@163.com>
2021-10-09 17:37:18 +0800

    删除printf输出

--- User/Hardware/src/can1_device.c
+++ User/Hardware/src/can1_device.c
@@ -1,12 +1,12 @@
 #include "can1_device.h"
 
-static Pid_Position_t motor_fl_speed_pid = NEW_POSITION_PID(11, 0, 5, 2000, 16000, 0, 1000, 500);
-static Pid_Position_t motor_fr_speed_pid = NEW_POSITION_PID(11, 0, 5, 2000, 16000, 0, 1000, 500);
-static Pid_Position_t motor_bl_speed_pid = NEW_POSITION_PID(11, 0, 5, 2000, 16000, 0, 1000, 500);
-static Pid_Position_t motor_br_speed_pid = NEW_POSITION_PID(11, 0, 5, 2000, 16000, 0, 1000, 500);
+static Pid_Position_t motor_fl_speed_pid = NEW_POSITION_PID(11, 0, 4.5, 2000, 16000, 0, 1000, 500);
+static Pid_Position_t motor_fr_speed_pid = NEW_POSITION_PID(11, 0, 4.5, 2000, 16000, 0, 1000, 500);
+static Pid_Position_t motor_bl_speed_pid = NEW_POSITION_PID(11, 0, 4.5, 2000, 16000, 0, 1000, 500);
+static Pid_Position_t motor_br_speed_pid = NEW_POSITION_PID(11, 0, 4.5, 2000, 16000, 0, 1000, 500);
 
 void Set_ChassisMotor_Speed(float speed_fl, float speed_fr, float speed_bl, float speed_br, Motor_Measure_t *chassis_motor_feedback_data)
-{
+{ 
     int16_t a = Pid_Position_Calc(&motor_fl_speed_pid, speed_fl, chassis_motor_feedback_data[0].speed_rpm);
     int16_t b = Pid_Position_Calc(&motor_fr_speed_pid, speed_fr, chassis_motor_feedback_data[1].speed_rpm);
     int16_t c = Pid_Position_Calc(&motor_bl_speed_pid, speed_bl, chassis_motor_feedback_data[2].speed_rpm);
@@ -17,8 +17,4 @@ void Set_ChassisMotor_Speed(float speed_fl, float speed_fr, float speed_bl, floa
         b,
         c,
         d);
-    // debug_log("%d %d %d %d\r\n", chassis_motor_feedback_data[0].speed_rpm,
-    //           chassis_motor_feedback_data[0].speed_rpm,
-    //           chassis_motor_feedback_data[0].speed_rpm,
-    //           chassis_motor_feedback_data[0].speed_rpm);
 }
