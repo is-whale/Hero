a3a156e2c9c275bcd63f000c760bd8aa3ce65f50
weihao <weihao_ysgs@163.com>
2021-10-10 21:55:46 +0800

    测试波轮通过

--- User/Task/src/wave_wheel_task.c
+++ User/Task/src/wave_wheel_task.c
@@ -9,7 +9,7 @@ static const int32_t *fire_one_bullet_signal;
 static const int32_t *fire_five_bullet_signal;
 
 static float wave_motor_speed = 0.0;
-static const uint32_t wave_once_machine_angle = 50000;
+static const int32_t wave_once_machine_angle = -31488;
 
 static void Emission_Once_Time(void);
 
@@ -54,7 +54,7 @@ static void Emission_Once_Time(void)
 	wave_motor_speed = Calc_Wave_Motor_Angle8191_Pid(wave_once_machine_angle, *angle_integral);
 
 	///< 初步测试，发射一颗弹丸的 pid 计算时间最多是 320 ms,时间在 290-320 之间徘徊
-	while ((fabs(wave_motor_speed) > 10) && (*angle_integral < 119900))
+	while ((fabs(wave_motor_speed) > 10) && (*angle_integral > -31300))
 	{
 		start_time = osKernelSysTick();
 		wave_motor_speed = Calc_Wave_Motor_Angle8191_Pid(wave_once_machine_angle, *angle_integral);
@@ -70,12 +70,12 @@ static void Emission_Once_Time(void)
 		end_time = osKernelSysTick();
 		cal_pid_time += (end_time - start_time);
 		///< 是否使用 pid 计算时间作为结束条件待决定
-		if (cal_pid_time > 340)
-		{
-			*angle_integral = 0;
-			wave_motor_speed = 0.0;
-			break;
-		}
+		// if (cal_pid_time > 340)
+		// {
+		// 	*angle_integral = 0;
+		// 	wave_motor_speed = 0.0;
+		// 	break;
+		// }
 	}
 	*angle_integral = 0;
 	wave_motor_speed = 0.0;
